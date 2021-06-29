87deac20a74dc5c567ee519ca4ed0526784ccb87
Aaron <aaron@MacBook-Air.local>
2021-06-29 15:14:37 -0500

    First commit for studio menu project.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public string FoodCatagories { get; set; }
+        public Menu(string afood)
+        {
+            FoodCatagories = afood;
+        }
+        public static string GetTimeStamp(DateTime value)
+        {
+            return value.ToString("yyyy/MM/dd/HHmm");
+        }
+
+
+        //new food item
+
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string Food { get; set; }
+        public string FoodDescription { get; set; }
+        public double FoodPrice { get; set; }
+        public MenuItem(string afood, string afoodDescription, double afoodPrice)
+        {
+            Food = afood;
+            FoodDescription = afoodDescription;
+            FoodPrice = afoodPrice;
+        }
+
+   
+    }
+}
